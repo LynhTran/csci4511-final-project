e773c8fd7fac5005aa2f80b2036d5e6ba224d560
lynhtran <lynht273@gmail.com>
2022-11-28 15:52:52 -0600

    auto train with our spam dataset - needs testing

--- auto_train.py
+++ auto_train.py
@@ -4,9 +4,25 @@ import sklearn.datasets
 import sklearn.metrics
 
 if __name__ == "__main__":
-    X, y = sklearn.datasets.load_digits(return_X_y=True)
-    X_train, X_test, y_train, y_test = \
-        sklearn.model_selection.train_test_split(X, y, random_state=1)
+    classes = ['not_spam', 'spam']
+
+    d1 = pd.read_json(os.path.join('SpamDataset', 'ham_easy.json'))
+    d2 = pd.read_json(os.path.join('SpamDataset', 'ham_hard.json'))
+    d3 = pd.read_json(os.path.join('SpamDataset', 'spam.json'))
+
+    data = pd.concat([d1, d2, d3])
+
+    corpus = data['content']
+    classes = data['class']
+
+    count_vect = CountVectorizer()
+    X_train_counts = count_vect.fit_transform(corpus)
+
+    tfidf_transformer = TfidfTransformer()
+    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+
+    X_train, X_test, y_train, y_test = train_test_split(X_train_tfidf, classes, test_size=0.20, random_state=10)
+    
     automl = autosklearn.classification.AutoSklearnClassifier()
     automl.fit(X_train, y_train)
     y_hat = automl.predict(X_test)
